d9e95cdc89f018cca09c0ec189bfffec059fb7d1
P3rl4Dev <yocrackgta@gmail.com>
2021-11-17 12:40:52 +0100

    Aded Singup template

---
+++ p3rl4vault/src/components/Singup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Singup.css'
+
+
+function Singup() {
+    return (
+        <div>
+            <h1>jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
+        </div>
+    )
+}
+
+export default Singup
