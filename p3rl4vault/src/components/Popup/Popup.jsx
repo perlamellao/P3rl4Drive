8debeb0084ea4110bbf027f5038a19f4f62542b7
perlamellao <yocrackgta@gmail.com>
2022-03-22 15:22:37 +0000

    Minor Changes

---
+++ p3rl4vault/src/components/Popup/Popup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Popup.css'
+
+function Popup({ message }) {
+    return (
+        <div className="alert alert-danger d-flex align-items-center" role="alert">
+            <div className="msg">
+                <i className='bx bxs-error icon'></i>
+                <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{message}</span>
+            </div>
+        </div>
+    )
+}
+
+export default Popup
